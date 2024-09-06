e127d8a2255368420f564c4906884e200371319f
binbinher <binbin.he515@outlook.com>
2024-09-06 11:56:06 +0200

    add tsk9

---
+++ python-if_else_loops_functions/9-main.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+print_last_digit = __import__('9-print_last_digit').print_last_digit
+
+print_last_digit(2004)
+print_last_digit(43455)
+r = print_last_digit(-10234)
+print(r)
