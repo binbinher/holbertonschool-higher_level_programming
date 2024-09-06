298bd425b15f07ffa498484008b3d3e913f59a9b
binbinher <binbin.he515@outlook.com>
2024-09-06 23:04:40 +0200

    add tsk3

---
+++ python-import_modules/3-infinite_add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import sys
+if __name__ == "__main__":
+    from sys import argv
+    args = sys.argv[1:]
+    addition = 0
+
+    for numbers in args:
+        addition += int(numbers)
+
+    print(addition)
