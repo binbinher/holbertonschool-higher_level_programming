f3ed634bfef43cb09045b0578f665e3868c2d614
binbinher <binbin.he515@outlook.com>
2024-09-06 13:45:49 +0200

    update

--- python-if_else_loops_functions/11-pow.py
+++ python-if_else_loops_functions/11-pow.py
@@ -1,3 +1,14 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
+
 def pow(a, b):
+    """
+    Compute the power of a number.
+
+    Args:
+        a (int): The base number.
+        b (int): The exponent.
+
+    Returns:
+        int: The result of raising `a` to the power of `b`.
+    """
     return a ** b
