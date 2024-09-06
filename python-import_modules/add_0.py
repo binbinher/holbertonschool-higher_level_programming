81ceae5477c83bd9b743228ecaa78d10f875c097
binbinher <binbin.he515@outlook.com>
2024-09-06 18:01:31 +0200

    update add_0

--- python-import_modules/add_0.py
+++ python-import_modules/add_0.py
@@ -1,11 +1,2 @@
 def add(a, b):
-    """My addition function
-
-    Args:
-        a: first integer
-        b: second integer
-
-    Returns:
-        The return value. a + b
-    """
     return a + b
