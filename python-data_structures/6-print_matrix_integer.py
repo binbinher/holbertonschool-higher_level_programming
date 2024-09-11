a8cb7c46ebf022883d1482a8b233070c99ae380d
binbinher <binbin.he515@outlook.com>
2024-09-11 11:47:07 +0200

    update style

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-     for row in matrix:
+    for row in matrix:
         for column in range(len(row)):
             print("{:d}".format(row[column]), end="")
             if column < len(row) - 1:
