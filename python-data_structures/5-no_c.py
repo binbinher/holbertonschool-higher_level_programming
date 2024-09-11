dd54bf0ef87d1b72da62e928eb03c10784e9bb57
binbinher <binbin.he515@outlook.com>
2024-09-11 11:44:56 +0200

    update task5 style

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -3,5 +3,5 @@ def no_c(my_string):
     new_string = []
     for char in my_string:
         if char != "c" and char != "C":
-                new_string.append(char)
+            new_string.append(char)
     return "".join(new_string)
