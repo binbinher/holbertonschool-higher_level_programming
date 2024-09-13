96085ce89d782ad50f71f672e39bc517150f148c
binbinher <binbin.he515@outlook.com>
2024-09-13 10:30:36 +0200

    update using append

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    new_list= [replace if x == search else x for x in my_list]
-    return new_list
+    new_list = []
+    for number in my_list:
+        if number == search:
+            new_list.append(replace)
+        else:
+            new_list.append(number)
+    return new_list    
