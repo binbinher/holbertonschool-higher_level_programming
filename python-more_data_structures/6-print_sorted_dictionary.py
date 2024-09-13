4f66795f86364b42476ac0478a931d98fbb5e3e4
binbinher <binbin.he515@outlook.com>
2024-09-13 19:34:27 +0200

    update 6

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
+
+
 def print_sorted_dictionary(a_dictionary):
     sorted_dict = dict(sorted(a_dictionary.items()))
     for key, value in sorted_dict.items():
-        print(f"{key}:{value}")
+        print(f"{key}: {value}")
