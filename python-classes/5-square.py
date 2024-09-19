e76e45424f444c8bbc994f315a21fd40646f577d
binbinher <binbin.he515@outlook.com>
2024-09-19 14:04:51 +0200

    udpate tsk5 documentation

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -1,22 +1,34 @@
 #!/usr/bin/python3
+"""a square class"""
+
+
 class Square:
+    "a square class define the square"
+
     def __init__(self, size =0):
+        """private instance attribute"""
         self.__size = size
 
+    @property
     def size(self):
+        """get the size of the square"""
         return self.__size
-
+    @size.setter
     def size(self, value):
+        """set the size of the square"""
         if type(value) is not int:
             raise TypeError("size must be an integer")
         elif value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
+
     def area(self):
+        """calbulate the area of square"""
         return self.__size**2
-    
+
     def my_print(self):
+        """print the square"""
         if self.__size == 0:
             print ()
-        for _ in range(self.__size):
+        for row in range(self.size):
             print("#" * self.__size)
