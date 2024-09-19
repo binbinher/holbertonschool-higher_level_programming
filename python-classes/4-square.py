f7853f7ad7664b860ce805ce22a24503700f66a7
binbinher <binbin.he515@outlook.com>
2024-09-19 13:58:35 +0200

    update tsk4 documentation

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -1,15 +1,22 @@
 #!/usr/bin/python3
+"""a class square"""
+
+
 class Square:
+    """a square class that defind a square"""
+
     def __init__(self, size=0):
         """instance attribute"""
         self.size = size
+
     @property
     def size(self):
         """get the size of the square"""
         return self.__size
-    
+
     @size.setter
     def size(self, value):
+        """set the size of the square"""
         if type(value) is not int:
             raise TypeError("size must be an integer")
         elif value < 0:
@@ -17,4 +24,5 @@ class Square:
         self.__size = value
 
     def area(self):
+        """calculate area of square"""
         return self.__size**2
