12f465140168d6dd096211bd570143356f85984b
binbinher <binbin.he515@outlook.com>
2024-09-19 13:51:58 +0200

    update task 2 documentation

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -3,7 +3,7 @@
 
 
 class Square:
-    """A Square class that devine a square"""
+    """A Square class that define a square"""
 
     def __init__(self, size=0):
         """private instance attribute"""
--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,10 +1,16 @@
 #!/usr/bin/python3
+"""A class Square"""
+
+
 class Square:
+    """A Square class that devine a square"""
+    
     def __init__(self, size=0):
         self.__size = size
         if type(size) is not int:
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
+        """private instance attribute"""
         self.__size = size
         
