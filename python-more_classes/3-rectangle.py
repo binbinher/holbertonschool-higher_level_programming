3cb1d75893c9a7e4807bf555123ecf299a62e533
binbinher <binbin.he515@outlook.com>
2024-09-20 16:34:01 +0200

    add tsk3

---
+++ python-more_classes/3-rectangle.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+"""this is for a class rectangle"""
+
+
+class Rectangle:
+    """class that defines a rectangle"""
+
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    def __str__(self):
+        my_rectangle = ""
+        if self.__height != 0 and self.__width != 0:
+            for row in range(self.__height):
+                my_rectangle += '#' * self.width
+                if row < self.__height - 1:
+                    my_rectangle += "\n"
+        return my_rectangle
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must >= 0")
+        self.__height = value
+
+    def area(self):
+        return self.__width * self.__height
+
+    def perimeter(self):
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        return (self.__width + self.__height) * 2
+    
