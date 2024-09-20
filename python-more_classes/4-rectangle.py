e4aff172a82b4e41523dc91d1aa95678ea264cd2
binbinher <binbin.he515@outlook.com>
2024-09-20 18:33:46 +0200

    add private attribute in repr and str

--- python-more_classes/4-rectangle.py
+++ python-more_classes/4-rectangle.py
@@ -19,7 +19,7 @@ class Rectangle:
         return my_rectangle
 
     def __repr__(self):
-        return f"Rectangle({self.__width},{self.__height})"
+        return f"Rectangle({self.width},{self.height})"
 
     @property
     def width(self):
