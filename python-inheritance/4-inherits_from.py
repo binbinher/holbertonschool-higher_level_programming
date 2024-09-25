40d0a68fb715a9a46f4c69013cfbdac39cbcad91
binbinher <binbin.he515@outlook.com>
2024-09-25 09:58:37 +0200

    correct function

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,13 +1,19 @@
 #!/usr/bin/python3
 """
-a module contains a function that return true or false
-if the object if an instance of a class that inheried from the class
+a module contains an inherited class-checking function.
 """
 
 def inherits_from(obj, a_class):
     """
-    a function that returns True if the object 
-    is an instance of a class that inherited 
-    from the specified class ; otherwise False.
+    checks if an object is an inherited instance of a class.
+
+    Arg:
+        obj(any): The object to check.
+        a_class(tpye): the class to match the type of obj to.
+    Returns:
+        If obj is an inherited instance of a_class -True.
+        Otherwise -False.
     """
-    return type(obj) is not a_class and isinstance(ojb, a_class)
+    if issubclass(type(obj), a_class) and type(obj) != a_class:
+        return True
+    return False
