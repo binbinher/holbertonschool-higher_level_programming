5080454a3364a540251f5c4d8e5b061b0bf70ddb
binbinher <binbin.he515@outlook.com>
2024-09-25 09:25:07 +0200

    update style

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 """
-a module contains Mylist class 
-inherites from list
+a module contains Mylist class inherites from list
 """
 
 class MyList(list):
-    """class that inherits from list"""
+    """
+    class that inherits from list
+    """
     def print_sorted(self):
         print(sorted(self))
