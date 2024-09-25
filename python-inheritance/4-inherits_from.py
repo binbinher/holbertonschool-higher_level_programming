79d6a601ea0442bebef777822cb3f99bdc499c08
binbinher <binbin.he515@outlook.com>
2024-09-25 10:19:09 +0200

    update4

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -7,6 +7,13 @@ Defines a class and inherited class-checking function.
 def inherits_from(obj, a_class):
     """
     checks if an object is an inherited instance of a class.
+
+    Args:
+        obj (any): The object to check.
+        a_class (type): The class to match the type of obj to.
+    Returns:
+        If obj is an inherited instance of a_class - True.
+        Otherwise - False.
     """
     if issubclass(type(obj), a_class) and type(obj) is not a_class:
         return True
