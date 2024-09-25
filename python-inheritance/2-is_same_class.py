542229d3139b2c6d416b26ec2b1e35e0966256c3
binbinher <binbin.he515@outlook.com>
2024-09-25 09:44:08 +0200

    fix style

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
-"""a module that contains a function that returns True
-if the object is exactly an instance of the specified class"""
+"""a module that contains a function is_same_class"""
 
 def is_same_class(obj, a_class):
     """
-    fucntion return True or false to check 
-    the object is an instance of a specified class or not
+    function that return True if an object is
+    exactly an instance of a specified class
     """
     return type(obj) is a_class
