2ee7d36a2621b03d5f6ea1a0636041fe0274f74f
binbinher <binbin.he515@outlook.com>
2024-09-25 09:24:07 +0200

    update style

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-"""a module contains Mylist class inherites from list"""
+"""
+a module contains Mylist class 
+inherites from list
+"""
 
 class MyList(list):
     """class that inherits from list"""
