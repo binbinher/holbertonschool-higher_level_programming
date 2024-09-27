ce36260feed52ad2ca0428a7552aeeec861e6d1e
binbinher <binbin.he515@outlook.com>
2024-09-27 21:11:51 +0200

    fix style

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -20,4 +20,4 @@ class Square(Rectangle):
 
     def __str__(self):
         """this method returns a strings representation"""
-        return"[Square] {}/{}".format(self.__size, self.__size)
+        return "[Square] {}/{}".format(self.__size, self.__size)
