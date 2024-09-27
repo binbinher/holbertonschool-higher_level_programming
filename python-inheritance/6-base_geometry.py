a85401b469432a69c739e0b7ce1a67a6afc3e3a4
binbinher <binbin.he515@outlook.com>
2024-09-27 14:05:28 +0200

    add tsk6

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+a module that contains a an empty class"""
+
+
+class BaseGeometry:
+    """
+    an empty class of BaseGeometry
+    """
+
+    def area(self):
+        """not implemented"""
+        raise Exception("area() is not implemente")
