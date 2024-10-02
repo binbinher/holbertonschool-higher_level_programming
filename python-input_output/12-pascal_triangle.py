8df23e47447d81baaa50da5ccadccfd7ed95e165
binbinher <binbin.he515@outlook.com>
2024-10-02 18:32:04 +0200

    fix style

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
-"""a module contains a function pascal_triangle"""
+"""This module contain a function pascal_triangle"""
 
 
 def pascal_triangle(n):
-    """a function that returns a list of lists of integers"""
+    """function that generate a Pascal's triangle"""
     if n <= 0:
         return []
 
     triangle = [[1]]
 
-    for i in range(1,n):
+    for i in range(1, n):
         row = [1]
         for j in range(1, i):
             row.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
