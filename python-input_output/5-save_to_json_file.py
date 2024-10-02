a440139865a52f83eb77889aeca4f73c1a3d8b82
binbinher <binbin.he515@outlook.com>
2024-10-02 14:13:42 +0200

    delete exception

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -9,11 +9,6 @@ def save_to_json_file(my_obj, filename):
     args:
     my_obj: the object to serialize and write to a file
     filename(str): the name of the file to save the JSON data.
-    Raises:
-    exception: any exception that might occur during file writing.
     """
-    try:
-        with open(filename, "w", encoding="utf8") as my_file:
-            json.dump(my_obj, my_file)
-    except Excetion as e:
-        print(f"Error saving to file {filename}:{e}")
+    with open(filename, "w", encoding="utf8") as my_file:
+        json.dump(my_obj, my_file)
