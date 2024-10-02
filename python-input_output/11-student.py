bba92c8e776bd9fac7afa409ccc04cabfea85f25
binbinher <binbin.he515@outlook.com>
2024-10-02 17:46:27 +0200

    fix

--- python-input_output/11-student.py
+++ python-input_output/11-student.py
@@ -1,20 +1,33 @@
- #!/usr/bin/python3
+ #!/usr/bin/python3#!/usr/bin/python3
 """
-a module contains a class Student
+A module that contains a class Student.
 """
-
 
 class Student:
-    """a class defines a student"""
+    """A class that defines a student."""
 
     def __init__(self, first_name, last_name, age):
-        """initializing a new student instance"""
+        """Initializes a new Student instance.
+        
+        Args:
+            first_name (str): The student's first name.
+            last_name (str): The student's last name.
+            age (int): The student's age.
+        """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        """retriving a dictionary representing the student"""
+        """Retrieves a dictionary representation of the Student instance.
+      
+        Args:
+            attrs (list, optional): A list of attribute names to retrieve.
+                                    If None, all attributes are retrieved.
+        
+        Returns:
+            dict: A dictionary containing the Student instance's attributes.
+        """
         if attrs is None:
             return self.__dict__
         else:
@@ -22,8 +35,13 @@ class Student:
                 attr: self.__dict__[attr]
                 for attr in attrs if attr in self.__dict__
             }
- 
-    def reload_from_json(self, json):
-        """public method replaces all attributes of the studnet instance"""
-        for key, value in json.items():
+
+    def reload_from_json(self, json_dict):
+        """Replaces all attributes of the Student instance with those from a dictionary.
+        
+        Args:
+            json_dict (dict): A dictionary where keys match attribute names and
+                              values are the new values for those attributes.
+        """
+        for key, value in json_dict.items():
             setattr(self, key, value)
