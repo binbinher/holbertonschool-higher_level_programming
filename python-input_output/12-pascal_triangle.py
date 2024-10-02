67468aecad530ffebf039ca1f900848c6c5a9ec6
binbinher <binbin.he515@outlook.com>
2024-10-02 18:23:07 +0200

    add tsk12

---
+++ python-input_output/12-pascal_triangle.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+a module contains a function pascal_triangle
+"""
+
+
+def pascal_triangle(n):
+    """a function that returns a list of lists of integers"""
+    if n <= 0:
+        return []
+
+    triangle = [[1]]
+
+    for i in range(1,n):
+        row = [1]
+        for j in range(1, i):
+            row.append(triangle[i - 1][j - 1]) + triangle[i - 1][j]
+        row.append(1)
+        triangle.append(row)
+
+    return triangle
