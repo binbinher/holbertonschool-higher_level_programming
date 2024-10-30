d7011db68c1b8bb108a42bdcd0e2596da8888727
binbinher <binbin.he515@outlook.com>
2024-10-30 11:02:01 +0100

    add tsk7

---
+++ SQL_more_queries/7-cities.sql
@@ -0,0 +1,21 @@
+-- this script creates database hbtn_0d_usa and table cities
+-- cities id INT unique auto generated, not null and primary key
+
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+
+USE DATABASE hbtn_0d_usa;
+
+-- Create the states table if it doesn't already exist
+CREATE TABLE IF NOT EXISTS states (
+    id INT AUTO_INCREMENT UNIQUE NOT NULL,
+		name VARCHAR(256) NOT NULL,
+		PRIMARY KEY(id)
+);
+
+CREATE TABLE IF NOT EXISTS cities (
+    id INT AUTO_INCREMENT UNIQUE NOT NULL,
+		state_id INT NOT NULL,
+		name VARCHAR(256) NOT NULL,
+		PRIMARY KEY (id),
+		FOREIGN KEY (state_id) REFERENCE states(id)
+);
