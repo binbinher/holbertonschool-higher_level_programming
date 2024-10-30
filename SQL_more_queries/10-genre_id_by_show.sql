bba9f243a74bad09bc8b0312440c79eec78b7550
binbinher <binbin.he515@outlook.com>
2024-10-30 23:52:55 +0100

    forgot comma after tv_shows.title

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,7 +1,7 @@
 -- this script lists all shows contained in hbtn_0d_tvshows that has at least one genre linked
 
 SELECT
-    tv_shows.title
+    tv_shows.title,
 		tv_show_genres.genre_id
 FROM
     tv_shows,
