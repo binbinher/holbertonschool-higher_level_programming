f625c1636c94dc996ad838ac3ef76be0a65870b0
binbinher <binbin.he515@outlook.com>
2024-10-30 23:54:58 +0100

    change where to join

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -6,8 +6,8 @@ SELECT
 FROM
     tv_shows,
 		tv_show_genres
-WHERE
-    tv_shows.id = tv_show_genres.show_id
+JOIN
+    tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 ORDER BY
     tv_shows.title ASC,
 		tv_show_genres.genre.id ASC;
