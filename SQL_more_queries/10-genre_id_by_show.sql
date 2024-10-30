019c27d4b0a5a6208672f206bd999c359c6b66df
binbinher <binbin.he515@outlook.com>
2024-10-30 23:50:45 +0100

    fix typo of tv_shows

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,13 +1,13 @@
 -- this script lists all shows contained in hbtn_0d_tvshows that has at least one genre linked
 
 SELECT
-    tv_show.title
+    tv_shows.title
 		tv_show_genres.genre_id
 FROM
     tv_shows,
 		tv_show_genres
 WHERE
-    tv_show.id = tv_show_genres.show_id
+    tv_shows.id = tv_show_genres.show_id
 ORDER BY
     tv_shows.title ASC,
 		tv_show_genres.genre.id ASC;
