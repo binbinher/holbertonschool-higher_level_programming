c9b8b863a6b8c7a2deb4678d114276420158baa0
binbinher <binbin.he515@outlook.com>
2024-10-30 10:25:08 +0100

    adding another explication line at the end of the script

--- SQL_more_queries/4-never_empty.sql
+++ SQL_more_queries/4-never_empty.sql
@@ -1,5 +1,8 @@
 -- this script creates table id_not_null
-CREATE TABLE IF NOT EXISTS id_not_null(
+
+CREATE TABLE IF NOT EXISTS id_not_null (
     id INT DEAFUT 1,
 		name VARCHAR(256)
 );
+
+-- this line encures that the table will have the two columns and their requirements.
