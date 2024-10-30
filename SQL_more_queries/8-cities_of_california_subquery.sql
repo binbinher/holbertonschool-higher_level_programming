07bc907ecfe90a3a516599334ac72d2a8ec14818
binbinher <binbin.he515@outlook.com>
2024-10-30 11:23:16 +0100

    add tsk8

---
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,6 @@
+-- this script lists all the cities of california that can be found in the database
+
+SELECT id, name
+FROM cities
+WHERE state_id = (SELECT id FRAM states WHERE name = 'California')
+ORDER BY id ASC;
