bf779526cf7bf2f6a1da47bb0342f79bc1573b93
binbinher <binbin.he515@outlook.com>
2024-10-31 13:17:22 +0100

    put the on row on lelf join row

--- SQL_more_queries/11-genre_id_all_shows.sql
+++ SQL_more_queries/11-genre_id_all_shows.sql
@@ -9,8 +9,7 @@ SELECT
 FROM
     tv_shows
 LEFT JOIN
-    tv_show_genres
-ON tv_show.id = tv_show_genres.show_id
+    tv_show_genres ON tv_show.id = tv_show_genres.show_id
 ORDER BY
     tv_shows.title ASC,
 		tv_show_genres.genre_id ASC;
