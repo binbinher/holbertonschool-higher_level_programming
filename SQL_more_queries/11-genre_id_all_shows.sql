0c51fa8d1d2709526afcfffbafb7d6d7fe996323
binbinher <binbin.he515@outlook.com>
2024-10-31 13:12:54 +0100

    add tsk11

---
+++ SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,16 @@
+-- this script lists all shows contained in the database hbtn_0d_tvshows.
+-- record should display tv_shows.title and tv_show_genres.genre_id
+-- sored in ascending order by both
+-- if it doesnt have a genre,display NULL
+
+SELECT
+    tv_shows.title,
+		tv_show_genres.genre_id
+FROM
+    tv_shows
+LEFT JOIN
+    tv_show_genres
+ON tv_show.id = tv_show_genres.show_id
+ORDER BY
+    tv_shows.title ASC,
+		tv_show_genres.genre_id ASC;
