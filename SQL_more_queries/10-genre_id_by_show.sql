744ef1a6507520ebff00f4b137e592ed8927207b
binbinher <binbin.he515@outlook.com>
2024-10-31 10:30:56 +0100

    join to where

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,12 +1,13 @@
--- this script lists all shows contained in hbtn_0d_tvshows that has at least one genre linked
-
-SELECT
-    tv_shows.title,
-		tv_show_genres.genre_id
-FROM
-    tv_shows
-JOIN
-    tv_show_genres ON tv_shows.id = tv_show_genres.show_id
-ORDER BY
-    tv_shows.title ASC,
-		tv_show_genres.genre.id ASC;
+-- Select the title of the TV shows and the genre_id from the tv_show_genres table
+-- Ensure the results are ordered by tv_shows.title and tv_show_genres.genre_id in ascending order
+SELECT 
+    tv_shows.title, 
+    tv_show_genres.genre_id 
+FROM 
+    tv_shows, 
+    tv_show_genres 
+WHERE 
+    tv_shows.id = tv_show_genres.show_id 
+ORDER BY 
+    tv_shows.title ASC, 
+    tv_show_genres.genre_id ASC;
