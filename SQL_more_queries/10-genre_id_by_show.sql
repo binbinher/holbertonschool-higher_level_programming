2255db521252cc3978c9e45b3a2d1a71f91a96a2
binbinher <binbin.he515@outlook.com>
2024-10-31 10:39:15 +0100

    try to use inner join

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,12 +1,14 @@
 -- Select the title of the TV shows and the genre_id from the tv_show_genres table
--- Ensure the results are ordered by tv_shows.title and tv_show_genres.genre_id in ascending order
+-- Ensure the results are ordered by tv 
+
 SELECT 
     tv_shows.title, 
     tv_show_genres.genre_id 
 FROM 
-    tv_shows, 
+    tv_shows
+INNER JOIN 
     tv_show_genres 
-WHERE 
+ON 
     tv_shows.id = tv_show_genres.show_id 
 ORDER BY 
     tv_shows.title ASC, 
