3573decd8bbc4c5fc2e848e5df8891093932fb3d
binbinher <binbin.he515@outlook.com>
2024-10-31 13:42:57 +0100

    add tsk12

---
+++ SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,11 @@
+-- this script lists all shows without a genre linked.
+-- Results are ordered by tv_shows.title and tv_show_genres.genre_id in ascending order
+SELECT 
+    tv_shows.title,
+		tv_show_genres.genre_id 
+FROM
+    tv_shows 
+LEFT JOIN 
+    tv_show_genres ON tv_shows.id = tv_show_genres.show_id 
+WHERE tv_show_genres_id IS NULL
+ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
