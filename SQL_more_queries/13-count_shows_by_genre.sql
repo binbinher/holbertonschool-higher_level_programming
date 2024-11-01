51255adc0cd6138f3e04100e9e69720042df0f3a
binbinher <binbin.he515@outlook.com>
2024-11-01 13:06:05 +0100

    try inner join again

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -2,11 +2,10 @@
 -- TVshow genre, number of shows linked to this genre should be displayed
 -- sorted in descentding order by number of shows
 
-SELECT tv_genres.names AS genre, COUNT(tv_show_genres.show_id) AS number_of_shows
+SELECT tv_genres.names AS genre,
+       COUNT(tv_show_genres.show_id) AS number_of_shows
 FROM tv_genres
--- Join the tv_genres table with the tv_show_genres table
-LEFT JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
-GROUP BY tv_genres.id, tv_genres.names
--- Only include genres that have at least one show
+INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+GROUP BY tv_genres.names
 HAVING COUNT(tv_show_genres.show_id) > 0
 ORDER BY number_of_shows DESC;
