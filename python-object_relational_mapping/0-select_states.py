47c606b2bbdfc23e18a572f7efd228a68a7b3ee2
binbinher <binbin.he515@outlook.com>
2024-11-01 20:57:37 +0100

    update style

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
 
+"""
+This module connects to a MySQL database and retrieves all states
+from the specified database. The results are printed in ascending
+order by state ID.
+"""
+
 import MySQLdb
 import sys
 
@@ -7,25 +13,25 @@ if len(sys.argv) != 4:
     print("Usage: ./0-get_all_states.py <mysql username> <mysql password> <database name>")
     sys.exit(1)
 
-# Get arguments from command line
+"""Get arguments from command line"""
 mysql_username = sys.argv[1]
 mysql_password = sys.argv[2]
 database = sys.argv[3]
 
-# connect to MYSQL
+"""connect to MYSQL"""
 db = MySQLdb.connect(host="localhost", user=mysql_username, passwd=mysql_password, db=database)
 cursor = db.cursor()
 
-# Execute the SQL query to retrieve all states
+"""Execute the SQL query to retrieve all states"""
 cursor.execute("SELECT * FROM states ORDER BY id ASC")
 
-# Fetch all results
+"""Fetch all results"""
 states = cursor.fetchall()
 
-# Display results
+"""Display results"""
 for state in states:
     print(state)
 
-# Close the cursor and database connection
+"""Close the cursor and database connection"""
 cursor.close()
 db.close()
