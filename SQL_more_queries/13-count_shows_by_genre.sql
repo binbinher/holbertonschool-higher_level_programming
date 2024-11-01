4f3c8675d607e1e6fef49f44a8ef583b3d2f8c1a
binbinher <binbin.he515@outlook.com>
2024-11-01 13:04:04 +0100

    group by names as well

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -6,7 +6,7 @@ SELECT tv_genres.names AS genre, COUNT(tv_show_genres.show_id) AS number_of_show
 FROM tv_genres
 -- Join the tv_genres table with the tv_show_genres table
 LEFT JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
-GROUP BY tv_genres.id
+GROUP BY tv_genres.id, tv_genres.names
 -- Only include genres that have at least one show
 HAVING COUNT(tv_show_genres.show_id) > 0
 ORDER BY number_of_shows DESC;
