a7e0db9372b35da9440fa6c55cb5a2c5f771e39b
binbinher <binbin.he515@outlook.com>
2024-11-01 13:02:31 +0100

    arranging commentting

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,11 +1,12 @@
 -- this script lists all genres and display the number of shows linked to each
 -- TVshow genre, number of shows linked to this genre should be displayed
--- first column must be called genre, second is number_of_shows
 -- sorted in descentding order by number of shows
 
 SELECT tv_genres.names AS genre, COUNT(tv_show_genres.show_id) AS number_of_shows
 FROM tv_genres
+-- Join the tv_genres table with the tv_show_genres table
 LEFT JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
 GROUP BY tv_genres.id
+-- Only include genres that have at least one show
 HAVING COUNT(tv_show_genres.show_id) > 0
 ORDER BY number_of_shows DESC;
