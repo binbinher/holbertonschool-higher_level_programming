4d23a936357042fdace6fb1a13884aaa66b9781d
binbinher <binbin.he515@outlook.com>
2024-11-01 23:27:21 +0100

    fix style

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -8,7 +8,7 @@ from sys import argv
 
 # The code should not be executed when imported
 if __name__ == "__main__":
-  # make a connection to the database
+    # make a connection to the database
     db = MySQLdb.connect(
         host="localhost", port=3306, user=argv[1], passwd=argv[2], db=argv[3]
     )
@@ -25,4 +25,3 @@ if __name__ == "__main__":
     # Clean up process
     cur.close()
     db.close()
-    
