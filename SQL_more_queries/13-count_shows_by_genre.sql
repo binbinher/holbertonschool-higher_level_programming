d9e9f6e7d40794f4e449aea3c4785788c9098b35
binbinher <binbin.he515@outlook.com>
2024-11-01 12:56:40 +0100

    add tsk13

---
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,10 @@
+-- this script lists all genres and display the number of shows linked to each
+-- TVshow genre, number of shows linked to this genre should be displayed
+-- first column must be called genre, second is number_of_shows
+-- sorted in descentding order by number of shows
+
+SELECT tv_genres.names AS genre, COUNT(tv_show_genres.show_id) AS number_of_shows
+FROM tv_genres
+INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+GROUP BY tv_genres.names
+ORDER BY number_of_shows DESC;
