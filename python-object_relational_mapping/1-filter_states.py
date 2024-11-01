c630dac6066b423e1f1383eef5286690c7ef809a
binbinher <binbin.he515@outlook.com>
2024-11-01 22:34:15 +0100

    not using def main function

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -9,34 +9,32 @@ in ascending order by state ID.
 import MySQLdb
 import sys
 
-def main():
-    """Main function to execute the database query and print results."""
-    if len(sys.argv) != 4:
-        print("Usage: ./1-filter_states.py <mysql username> <mysql password> <database name>")
-        sys.exit(1)
+if len(sys.argv) != 4:
+    print("Usage: ./0-get_all_states.py <mysql username> <mysql password> <database name>")
+    sys.exit(1)
 
-    # Get arguments from command line
-    mysql_username = sys.argv[1]
-    mysql_password = sys.argv[2]
-    database = sys.argv[3]
+"""Get arguments from command line"""
+mysql_username = sys.argv[1]
+mysql_password = sys.argv[2]
+database = sys.argv[3]
 
-    # Connect to MySQL
-    db = MySQLdb.connect(host="localhost", user=mysql_username, passwd=mysql_password, db=database)
-    cursor = db.cursor()
+"""connect to MYSQL"""
+db = MySQLdb.connect(host="localhost", user=mysql_username, passwd=mysql_password, db=database)
+cursor = db.cursor()
 
-    # Execute the SQL query to retrieve states starting with 'N'
-    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+"""
+xecute the SQL query to retrieve states 
+starting with 'N'
+"""
+cursor.execute("SELECT * FROM states WHERE name like 'N%' ORDER BY id ASC")
 
-    # Fetch all results
-    states = cursor.fetchall()
+"""Fetch all results"""
+states = cursor.fetchall()
 
-    # Display results
-    for state in states:
-        print(state)
+"""Display results"""
+for state in states:
+    print(state)
 
-    # Close the cursor and database connection
-    cursor.close()
-    db.close()
-
-if __name__ == "__main__":
-    main()
+"""Close the cursor and database connection"""
+cursor.close()
+db.close()
