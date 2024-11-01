21a2fe9c6f95edf7f3b0bd536b4d3ace7afb8eb9
binbinher <binbin.he515@outlook.com>
2024-11-01 20:53:29 +0100

    delete function

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -3,35 +3,29 @@
 import MySQLdb
 import sys
 
-def get_all_states(username, password, database):
-    # Establish connection to the database
-    db = MySQLdb.connect(host="localhost", user=username, passwd=password, db=database, port=3306)
-    cursor = db.cursor()
+if len(sys.argv) != 4:
+    print("Usage: ./0-get_all_states.py <mysql username> <mysql password> <database name>")
+    sys.exit(1)
 
-    # Execute the SQL query to retrieve all states
-    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+# Get arguments from command line
+mysql_username = sys.argv[1]
+mysql_password = sys.argv[2]
+database = sys.argv[3]
 
-    # Fetch all results
-    states = cursor.fetchall()
+# connect to MYSQL
+db = MySQLdb.connect(host="localhost", user=mysql_username, passwd=mysql_password, db=database)
+cursor = db.cursor()
 
-    # Display results
-    for state in states:
-        print(state)
+# Execute the SQL query to retrieve all states
+cursor.execute("SELECT * FROM states ORDER BY id ASC")
 
-    # Close the cursor and database connection
-    cursor.close()
-    db.close()
+# Fetch all results
+states = cursor.fetchall()
 
-if __name__ == "__main__":
-    # Check if the script is run directly
-    if len(sys.argv) != 4:
-        print("Usage: ./0-get_all_states.py <mysql username> <mysql password> <database name>")
-        sys.exit(1)
+# Display results
+for state in states:
+    print(state)
 
-    # Get arguments from command line
-    mysql_username = sys.argv[1]
-    mysql_password = sys.argv[2]
-    database_name = sys.argv[3]
-
-    # Call the function to get all states
-    get_all_states(mysql_username, mysql_password, database_name)
+# Close the cursor and database connection
+cursor.close()
+db.close()
