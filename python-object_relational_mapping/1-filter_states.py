4e231d896b44ef6ee4ad50286764f8cdc2ebb36b
binbinher <binbin.he515@outlook.com>
2024-11-01 22:41:43 +0100

    make it less complicated

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,40 +1,14 @@
 #!/usr/bin/python3
 
-"""
-This script connects to a MySQL database and retrieves all states
-with a name starting with the letter 'N'. The results are displayed
-in ascending order by state ID.
-"""
+"""List all states starting with N"""
 
-import MySQLdb
 import sys
+import MySQLdb
 
-if len(sys.argv) != 4:
-    print("Usage: ./1-filter_states.py <mysql username> <mysql password> <database name>")
-    sys.exit(1)
-
-"""Get arguments from command line"""
-mysql_username = sys.argv[1]
-mysql_password = sys.argv[2]
-database = sys.argv[3]
-
-"""connect to MYSQL"""
-db = MySQLdb.connect(host="localhost", user=mysql_username, passwd=mysql_password, db=database)
-cursor = db.cursor()
-
-"""
-xecute the SQL query to retrieve states 
-starting with 'N'
-"""
-cursor.execute("SELECT * FROM states WHERE name like 'N%' ORDER BY id ASC")
-
-"""Fetch all results"""
-states = cursor.fetchall()
-
-"""Display results"""
-for state in states:
-    print(state)
-
-"""Close the cursor and database connection"""
-cursor.close()
-db.close()
+if __name__ == "__main__":
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT * FROM `states` WHERE BINARY name LIKE 'N%'")
+    states = cur.fetchall()
+    for state in states:
+        print(state)
