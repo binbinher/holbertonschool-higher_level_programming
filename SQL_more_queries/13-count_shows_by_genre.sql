9c22f14e0022dec90abe863414bc5410f812e188
binbinher <binbin.he515@outlook.com>
2024-11-01 13:00:06 +0100

    adding having condition

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -5,6 +5,7 @@
 
 SELECT tv_genres.names AS genre, COUNT(tv_show_genres.show_id) AS number_of_shows
 FROM tv_genres
-INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+LEFT JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
 GROUP BY tv_genres.id
+HAVING COUNT(tv_show_genres.show_id) > 0
 ORDER BY number_of_shows DESC;
